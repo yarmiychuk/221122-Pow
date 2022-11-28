0d7009a58ff4ff67a3ad6d17669e94d865e5afe5
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-11-29 02:39:39 +0300

    Second commit

--- Program.java
+++ Program.java
@@ -16,29 +16,26 @@ public class Program {
         int[] data = readData();
         int a = data[0];
         int b = data[1];
-        Integer result = getResult(a, b);
+        String result = getResult(a, b);
         writeResult(a, b, result);
     }
 
     public static void generateData() {
         Random r = new Random();
         int a = r.nextInt(MAX_NUMBER + 1);
-        int b = r.nextInt(MAX_POW + 1);
+        int b = r.nextInt(MAX_POW * 2 + 1) - MAX_POW;
         System.out.printf("a: %s, b: %s \n", a, b);
 
         boolean isAFirst = r.nextInt(2) == 0;
         String data;
-        if (isAFirst) {
-            data = "a " + a + "\nb " + b;
-        } else {
-            data = "b " + b + "\na " + a; 
-        }
+        if (isAFirst) data = "a " + a + "\nb " + b;
+        else data = "b " + b + "\na " + a;
 
         try (FileWriter fw = new FileWriter(FILE_INPUT, false)) {
             fw.write(data);
             fw.append("\n");
             fw.flush();
-            System.out.println("Файл успешно записан");
+            System.out.println("Файл с исходными значениями успешно записан");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -50,11 +47,8 @@ public class Program {
             String line;
             while ((line = br.readLine()) != null) {
                 int number = Integer.parseInt(line.substring(2, line.length()));
-                if (line.startsWith("a")) {
-                    data[0] = number;
-                } else {
-                    data[1] = number;
-                }
+                if (line.startsWith("a")) data[0] = number;
+                else data[1] = number;
             }
             br.close();
             System.out.println("Файл успешно прочитан");
@@ -64,22 +58,49 @@ public class Program {
         return data;
     }
 
-    public static Integer getResult(int a, int b) {
-        if (a == 0 && b == 0) return null;
-        return (int) Math.pow(a, b);
+    public static String getResult(int a, int b) {
+        if (a == 0 && b == 0) return "не определён";
+        if (b == 0) return "1";
+        if (b > 0) return String.valueOf(getPow(a, b));
+        else return String.valueOf(1 / getPow(a, b * -1));
     }
 
-    public static void writeResult(int a, int b, Integer result) {
+    public static double getPow(int a, int b) {
+        if (b == 1) return a;
+        if (b % 2 == 0) return getPow(a * a, b / 2);
+        else return a *= getPow(a * a, (b - 1) / 2);
+    }
+
+    public static void writeResult(int a, int b, String result) {
         try (FileWriter fw = new FileWriter(FILE_OUTPUT, false)) {
             String message = "Результат возведения числа " + a + " в степень " + b + "\n";
             fw.write(message);
-            if (result == null) fw.write("не определён");
-            else fw.write(String.valueOf(result));
+            fw.write(result);
             fw.append("\n");
             fw.flush();
             System.out.println(message + "записан в файл " + FILE_OUTPUT);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public static int[] readFile(String name) {
+        int[] num = new int[2];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(name));
+            String str;
+            while ((str = br.readLine()) != null) {
+                if (str.charAt(0) == 'a') {
+                    num[0] = Integer.parseInt(str.substring(3, str.length()));
+                }
+                if (str.charAt(0) == 'b') {
+                    num[1] = Integer.parseInt(str.substring(3, str.length()));
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        return num;
     }
 }
